3a91a250bedd07b5815d5a3ecdc8713e9106abe8
yen <bbbbbbb8910@gmail.com>
2024-12-26 23:07:07 +0800

    Feature NLog

---
+++ BlockChainStrategy.Library/Helpers/LoggerHelper.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+
+namespace BlockChainStrategy.Library.Helpers
+{
+    internal static class LoggerHelper
+    {
+        internal static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static void LogInfo(string message)
+        {
+            Logger.Info(message);
+            Console.WriteLine(message);
+        }
+
+        public static void LogError(string message)
+        {
+            Logger.Error(message);
+            Console.WriteLine(message);
+        }
+    }
+}
